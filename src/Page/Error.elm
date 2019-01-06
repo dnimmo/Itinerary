d237f0e26b84aad6a43cad3fa5dced054f98aad6
David Nimmo <dnimmo@gmail.com>
2019-01-06 15:28:12 +0000

    basic styling for error page

--- src/Page/Error.elm
+++ src/Page/Error.elm
@@ -1,7 +1,17 @@
 module Page.Error exposing (view) 
 
-import Element exposing (Element, text)
+import Element exposing (Element, centerX, centerY, column, padding, paragraph, spacing, text, rgb255)
+import Element.Font as Font
 
 view : String -> Element msg 
 view errorMessage =
-    text errorMessage
+        column
+        [ centerX
+        , centerY
+        , padding 50
+        , spacing 20
+        , Font.color <| rgb255 255 255 255
+        ]
+        [ paragraph [ centerX ] [ text "Unfortunately, something went wrong:" ]
+        , paragraph [ centerX ] [ text errorMessage]
+        ]
